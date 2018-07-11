a1a3e04bf2fdaadb865c4058b43e8c2856dc7bb8
Ryan Burn <ryan.burn@gmail.com>
2018-07-11 15:58:43 -0700

    Fill out more tests.

--- test/tracer.lua
+++ test/tracer.lua
@@ -6,10 +6,12 @@ function new_mocktracer(json_file)
   if mocktracer_path == nil then
     error("MOCKTRACER environmental variable must be set")
   end
-  return bridge_tracer:new(mocktracer_path, '{ "output_file":"' .. json_file .. '" }')
+  return bridge_tracer:new(
+            mocktracer_path, 
+            '{ "output_file":"' .. json_file .. '" }')
 end
 
-function readjson(file)
+function read_json(file)
     local f = assert(io.open(file, "rb"))
     local content = f:read("*all")
     f:close()
@@ -24,9 +26,21 @@ describe("in bridge_tracer", function()
       span = tracer:start_span("abc")
       span:finish()
       tracer:close()
-			json = readjson(json_file)
+			json = read_json(json_file)
 			assert.are.equal(#json, 1)
 			assert.are.equal(json[1]["operation_name"], "abc")
+    end)
+
+    it("supports customizing the time points", function()
+    end)
+
+    it("errors when passed an incorrect operation name", function()
+      json_file = os.tmpname()
+      tracer = new_mocktracer(json_file)
+      local errorfn = function()
+        span = tracer:start_span({})
+      end
+      assert.has_error(errorfn)
     end)
   end)
 end)
