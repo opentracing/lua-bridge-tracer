b3c78bd1ca82a5850637c768988ede3621f2c74e
Ryan Burn <ryan.burn@gmail.com>
2018-07-25 11:58:34 -0700

    Fix ASAN error.

--- ci/setup_build_environment.sh
+++ ci/setup_build_environment.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get install --no-install-recommends --no-install-suggests -y \
                 ca-certificates \
                 build-essential \
+                software-properties-common \
                 cmake \
                 pkg-config \
                 git \
@@ -18,5 +19,12 @@ apt-get install --no-install-recommends --no-install-suggests -y \
                 unzip \
                 libreadline6-dev \
                 libncurses5-dev \
+                gcc-8 \
+                g++-8 \
                 python python-setuptools python-pip
+
+### Use gcc-8 (the default gcc has this problem when using with address sanitizer:
+### https://gcc.gnu.org/bugzilla/show_bug.cgi?id=84428)
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+
 pip install gcovr
