b11d858b5504a80eaf2e378b36014858e123a7f8
Ryan Burn <ryan.burn@gmail.com>
2018-08-01 11:43:14 -0700

    Document example better.

--- example/tutorial/tutorial.lua
+++ example/tutorial/tutorial.lua
@@ -1,21 +1,26 @@
-bridge_tracer = require 'opentracing_bridge_tracer'
+-- Provides an example of basic usage of the opentracing_bridge_tracer
+--
+-- Run with
+--    lua tutorial.lua <path-to-tracer-plugin> <path-to-tracer-config>
+
+local bridge_tracer = require 'opentracing_bridge_tracer'
 
 assert(#arg == 2)
 
 -- initialize a tracer
-tracer_library = arg[1]
-config_file = arg[2]
+local tracer_library = arg[1]
+local config_file = arg[2]
 
-f = assert(io.open(config_file, "rb"))
-config = f:read("*all")
+local f = assert(io.open(config_file, "rb"))
+local config = f:read("*all")
 f:close()
 
-tracer = bridge_tracer:new(tracer_library, config)
+local tracer = bridge_tracer:new(tracer_library, config)
 
-parent_span = tracer:start_span("parent")
+local parent_span = tracer:start_span("parent")
 
 -- create a child span
-child_span = tracer:start_span(
+local child_span = tracer:start_span(
                     "ChildA", 
                     {["references"] = {{"child_of", parent_span:context()}}})
 
@@ -24,12 +29,12 @@ child_span:log_kv({["event"] = "simple log", ["abc"] = 123})
 child_span:finish()
 
 -- propagate the span context
-carrier = {}
+local carrier = {}
 tracer:text_map_inject(parent_span:context(), carrier)
-span_context = tracer:text_map_extract(carrier)
+local span_context = tracer:text_map_extract(carrier)
 assert(span_context ~= nil)
 
-propagation_span = tracer:start_span(
+local propagation_span = tracer:start_span(
                       "PropagationSpan", 
                       {["references"] = {{"follows_from", span_context}}})
 propagation_span:finish()
