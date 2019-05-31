1d9f3d52dffbb23bf9aa8887565c8663d59d57d0
Ryan Burn <ryan.burn@gmail.com>
2019-05-31 12:46:47 -0700

    Fix extraction for no span context.

--- src/dynamic_tracer.cpp
+++ src/dynamic_tracer.cpp
@@ -166,7 +166,7 @@ class DynamicTracer final : public opentracing::Tracer,
       return span_context_maybe;
     }
     if (*span_context_maybe == nullptr) {
-      return nullptr;
+      return std::unique_ptr<opentracing::SpanContext>{};
     }
     return std::unique_ptr<opentracing::SpanContext>{new DynamicSpanContext{
         shared_from_this(), std::move(*span_context_maybe)}};
